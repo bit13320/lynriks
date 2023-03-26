8281df5107b74e2846157235a1dd8b6601500856
bit13320 <128894026+bit13320@users.noreply.github.com>
2023-03-26 16:15:47 +0000

    v0.0.1 Madonna
    now supports scrolling lyrics
    bug: time stamp cant end with 0

---
+++ lrc_Madonna.js
@@ -0,0 +1,82 @@
+const lrcStr = `
+[00:00.01] 作词 : Andrew Lloyd Webber/Tim Rice
+[00:01.01] 作曲 : Andrew Lloyd Webber/Tim Rice
+[00:53.01]It won't be easy, you'll think it strange
+[00:53.01]那并非易事 你会为此而困惑
+[01:00.01]When I try to explain how I feel
+[01:00.01]当我试着表达自己的感情
+[01:06.01]That I still need your love after all that I've done
+[01:06.01]道着我仍渴求着你的爱 哪怕我已付出一切
+[01:13.01]You won't believe me
+[01:13.01]但你还是不相信我
+[01:18.01]All you will see is a girl you once knew
+[01:18.01]你认定了我还是从前的那个小女孩
+[01:24.01]Although she's dressed up to the nines
+[01:24.01]就算她打扮得体
+[01:29.01]At sixes and sevens with you
+[01:29.01]也还是与你格格不入
+[01:38.01]I had to let it happen, I had to change
+[01:38.01]我只能任它如此  我得改变
+[01:47.01]Couldn't stay all my life down at heel
+[01:47.01]我不愿把一生都葬在地狱里
+[01:53.01]Looking out of the window, staying out of the sun
+[01:53.01]终日张望 不见天日
+[02:00.01]So I chose freedom
+[02:00.01]所以我奔向自由
+[02:06.01]Running around trying everything new
+[02:06.01]四处环游，尝试一切新鲜事物
+[02:10.01]But nothing impressed me at all
+[02:10.01]但那些不过都是凡桃俗李
+[02:17.01]I never expected it to
+[02:17.01]非我所望
+[02:26.01]Don't cry for me Argentina
+[02:26.01]阿根廷，别为我哭泣
+[02:31.01]The truth is I never left you
+[02:31.01]我从未离你而去
+[02:36.01]All through my wild days
+[02:36.01]在那段狂野岁月
+[02:39.01]My mad existence
+[02:39.01]和疯狂历程之中
+[02:42.01]I kept my promise
+[02:42.01]我始终信守诺言
+[02:45.01]Don't keep your distance
+[02:45.01]别把我拒之千里
+[02:53.01]And as for fortune, and as for fame
+[02:53.01]不管是名与利
+[03:00.01]I never invited them in
+[03:00.01]我不曾有所求
+[03:06.01]Though it seemed to the world they were all I desired
+[03:06.01]尽管对全世界来说 它们正是我所渴望的
+[03:12.01]They are illusions
+[03:12.01]它们不过是泡影
+[03:16.01]They're not the solutions they promised to be
+[03:16.01]绝非如它们所承诺的那样 是一切的解决途径
+[03:22.01]The answer was here all the time
+[03:22.01]答案一直就在这儿
+[03:27.01]I love you and hope you love me
+[03:27.01]我深爱着你  我也希望你能爱我
+[03:36.01]Don't cry for me Argentina
+[03:36.01]阿根廷啊，别为我哭泣
+[04:09.01]Don't cry for me Argentina
+[04:09.01]阿根廷啊，别为我哭泣
+[04:14.01]The truth is I never left you
+[04:14.01]我从未离你而去
+[04:19.01]All through my wild days
+[04:19.01]在那段狂野岁月
+[04:22.01]My mad existence
+[04:22.01]和疯狂历程之中
+[04:25.01]I kept my promise
+[04:25.01]我始终信守诺言
+[04:28.01]Don't keep your distance
+[04:28.01]别把我拒之千里
+[04:35.01]Have I said too much?
+[04:35.01]我是不是说的太多了？
+[04:38.380]There's nothing more I can think
+[04:38.380]我已经想不起
+[04:41.01]of to say to you
+[04:41.01]任何字句了
+[04:49.01]But all you have to do is look at me to know
+[04:49.01]但只需你望我一眼 便可知每字每句
+[04:55.01]that everyword is true
+[04:55.01]皆是真情
+`
