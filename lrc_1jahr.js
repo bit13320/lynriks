6998984c8a76ed894fad91d31c9abea9ec99c130
bit13320 <bit13320505054@163.com>
2023-03-28 16:53:32 +0000

    update 0328 de

---
+++ lrc_1jahr.js
@@ -0,0 +1,67 @@
+const lrcStr = `[00:00.01] 作词 : Kai Kayef Fichtner
+[00:01.01] 作曲 : Kai Kayef Fichtner
+[00:05.81]Wache aus dem Traum
+[00:08.65]Ja, verdammt, Mann, ich glaub'
+[00:10.57]Du bist am Ende die Frau
+[00:12.23]Für eine Zukunft mit Kindern und Haus
+[00:14.09]Checke mein Handy, such' deine Nummer
+[00:15.52]Aber mit Sicherheit hast du 'ne neue
+[00:17.25]Mit Sicherheit hast du 'nen Freund jetzt
+[00:19.46]Ich habe es schon damals bereut, ey
+[00:21.33]Einmal zu viel nur gespielt
+[00:22.76]Falsche Entscheidung ma' wieder von mir
+[00:24.49]Nur um nach einem Jahr dann zu kapier'n
+[00:26.10]Dass mich hier keiner so krass fasziniert
+[00:27.89]Dass du mehr bist als all die andern
+[00:29.79]Ein Stern zwischen Diamanten
+[00:31.59]Vielleicht ruf' ich einfach mal an
+[00:33.21]Aber ich weiß es, du gehst halt nicht ran
+[00:35.07]Nachts um zwei
+[00:36.05]Fällt mir ein, es tut mir leid (yeah)
+[00:41.05]！在选项出现后8s内选择出下方缺失的歌词！
+[00:41.29]Letzte Nacht wacht' ich auf und ____ ich brauch' dich
+[00:41.29]昨晚我醒来，然后"发现"我想你（直译：需要你，brauch dich）
+[00:45.04]Brauch' dich bei mir (immer noch, immer noch)
+[00:48.04]Geh' raus und verlauf' mich, glaub', ich verlauf' mich
+[00:52.12]Vor deine Tür (immer noch, immer noch)
+[00:55.27]Aber du machst sie nicht auf und ich bleib' wieder draußen
+[00:59.25]Ja, verdammt, ich glaub', ich bin ein Jahr zu spät
+[01:02.55]Aber du machst sie nicht auf und ich bleib' wieder draußen
+[01:06.30]Ja, verdammt, ich glaub', ich bin ein Jahr zu spät
+[01:09.66]
+[01:09.87]Ey, mein Kopf voll Texten für eine WhatsApp-Massage
+[01:13.93]Aber Gott, ich schätze du hast mich schon vergessen
+[01:17.52]Denn ich bin ein Jahr zu spät, ein Jahr zu spät
+[01:21.01]Und ich kann's gar nicht glauben, wie schnell ein Jahr vergeht
+[01:24.59]Aber Baby, ich glaub', wir beide könnten eigentlich was werden
+[01:29.55]Lass uns nicht einsam sondern einfach zusamm'n sterben
+[01:33.12]Ich würd' dir zeigen, was ich mein', wenn du mich lässt, und
+[01:36.62]Vielleicht ist dein Herz sogar meine letzte Rettung
+[01:40.07](Oh-oh, oh-oh, oh-oh)
+[01:43.02]
+[01:43.25]Letzte Nacht wacht' ich auf und merkte ich brauch' dich
+[01:48.63]Brauch' dich bei mir (immer noch, immer noch)
+[01:48.63]！在选项出现后8s内选择出下方缺失的歌词！
+[01:51.60]Geh' raus und _____ mich, glaub', ich verlauf' mich
+[01:51.60]走到外走走，然后我迷路了，我认为我迷路了
+[01:55.63]Vor deine Tür (immer noch, immer noch)
+[01:55.63]走迷路在你的门前
+[01:58.65]Aber du machst sie nicht auf und ich bleib' wieder draußen
+[02:02.74]Ja, verdammt, ich glaub', ich bin ein Jahr zu spät
+[02:06.14]Aber du machst sie nicht auf und ich bleib' wieder draußen
+[02:09.80]Ja, verdammt, ich glaub', ich bin ein Jahr zu spät
+[02:13.15]
+[02:26.31]Letzte Nacht wacht' ich auf und merkte ich brauch' dich
+[02:26.31]！在选项出现后8s内选择出下方缺失的歌词！
+[02:30.87]Brauch' dich _____ mir
+[02:30.87]想要你在我身旁
+[02:33.47]Geh' raus und verlauf' mich, glaub', ich verlauf' mich
+[02:37.99]Vor deine Tür, yeah
+[02:41.60]Aber du machst sie nicht auf und ich bleib' wieder draußen
+[02:45.11]Ja, verdammt, ich glaub', ich bin ein Jahr zu spät
+[02:45.11]！在选项出现后8s内选择出下方缺失的歌词！
+[02:48.42]Aber du machst ____ nicht auf und ich bleib' wieder draußen
+[02:52.15]但是你不开门 我还是在门外（bleib：停留 draussen：外边，aus=out）
+[02:52.15]Ja, verdammt, ich glaub', ich bin ein Jahr zu spät
+[03:02.54]Ich bin ein Jahr zu spät
+[03:08.66]Ich bin ein Jahr zu spät`
