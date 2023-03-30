70a4d84acb80b85843e22298ac2d291c768b5f76
bit13320 <bit13320505054@163.com>
2023-03-30 15:42:34 +0000

    update 0330 03

--- lrc_первое_свидание.js
+++ lrc_первое_свидание.js
@@ -1,106 +1,111 @@
-const lrcStr = `[00:00.000]Alena Sergeevna Shvetsova
-[00:00.000]Швецова Алёна Сергеевна
-[00:00.840]Я приду, когда зацветет весна
-[00:00.840]I'll come when spring blooms
-[00:03.560]И покрасит в розовый собой облака
-[00:03.560]And it paints the clouds pink with itself
-[00:06.380]Соберу букет полевых цветов
-[00:06.380]I'll gather a bouquet of wildflowers
-[00:09.310]И спрячу
-[00:09.310]And hide
-[00:12.130]И мое сердечко болит от любви
-[00:12.130]And my heart aches from love
-[00:14.850]Просто слушай, ничего мне не говори
-[00:14.850]Just listen, don't say anything to me
-[00:17.670]Первое свидание последней весны
-[00:17.670]The first date of the last spring
-[00:20.380]Я плачу
-[00:20.380]I cry
-[00:23.310]Я приду, когда зацветет весна
-[00:23.310]I'll come when spring blooms
-[00:26.030]И покрасит в розовый собой облака
-[00:26.030]And it paints the clouds pink with itself
-[00:28.850]Соберу букет полевых цветов
-[00:28.850]I'll gather a bouquet of wildflowers
-[00:31.770]И спрячу
-[00:31.770]And hide
-[00:34.490]И мое сердечко болит от любви
-[00:34.490]And my heart aches from love
-[00:37.310]Просто слушай, ничего мне не говори
-[00:37.310]Just listen, don't say anything to me
-[00:40.130]Первое свидание последней весны
-[00:40.130]The first date of the last spring
-[00:42.850]Я плачу
-[00:42.850]I cry
-[00:45.670]В городе вчера зацвела сирень
-[00:45.670]Lilacs bloomed in the city yesterday
-[00:48.490]Сыпет лепестки и бросает тень
-[00:48.490]Scattering petals and casting a shadow
-[00:51.210]Словно в аниме заблестят зрачки
-[00:51.210]Pupils shine like in an anime
-[00:54.550]Невзаимно, ты прости
-[00:54.550]Unrequited, please forgive
-[00:56.850]В питерской квартире кто-то точит ножи
-[00:56.850]In Petersburg apartment some sharpens knives
-[00:59.780]Будущая жертва собирает цветы
-[00:59.780]The future victim gathers flowers
-[01:02.490]Встреча их случится, только ты подожди
-[01:02.490]Their meeting will happen, just wait
-[01:05.840]Подожди
-[01:05.840]Wait
-[01:08.140]Я приду, когда зацветёт весна
-[01:08.140]I'll come when spring blooms
-[01:10.850]И покрасит в розовый собой облака
-[01:10.850]And it paints the clouds pink with itself
-[01:13.670]Соберу букет полевых цветов
-[01:13.670]I'll gather a bouquet of wildflowers
-[01:16.390]И спрячу
-[01:16.390]And hide
-[01:19.420]И моё сердечко болит от любви
-[01:19.420]And my heart aches from love
-[01:22.140]Просто слушай, ничего мне не говори
-[01:22.140]Just listen, don't say anything to me
-[01:24.960]The first date of the last spring
-[01:24.960]Первое свидание последней весны
-[01:27.780]Я плачу
-[01:27.780]I cry
-[01:30.600]Сколько обнимал, сколько целовал
-[01:30.600]How many hugs, how many kisses
-[01:33.420]Украшал рисунками холодный подвал
-[01:33.420]Decorating a cold basement with drawings
-[01:36.240]Что же ты кричишь, не давая спать
-[01:36.240]Why are you screaming not letting me sleep
-[01:39.380]Ведь тебе не убежать
-[01:39.380]You cannot escape
-[01:41.890]Постучалась ветками сирень в окно
-[01:41.890]Lilac branches knocked on the window
-[01:44.600]Сыпет лепестки на бетонный пол
-[01:44.600]Scattering petals on the concrete floor
-[01:47.320]Сколько слез и радости ты нам принесла
-[01:47.320]How many tears and joy you brought us
-[01:50.660]Долгожданная весна
-[01:50.660]Long-awaited spring
-[01:55.890]Я приду, когда зацветёт весна
-[01:55.890]I'll come when spring blooms
-[01:58.600]И покрасит в розовый собой облака
-[01:58.600]And it paints the clouds pink with itself
-[02:01.430]Соберу букет полевых цветов
-[02:01.430]I'll gather a bouquet of wildflowers
-[02:04.250]И спрячу
-[02:04.250]And hide
-[02:06.960]And my heart aches from love
-[02:06.960]И мое сердечко болит от любви
-[02:09.890]Просто слушай, ничего мне не говори
-[02:09.890]Just listen, don't say anything to me
-[02:12.710]Первое свидание последней весны
-[02:12.710]The first date of the last spring
-[02:15.430]Я плачу
-[02:15.430]I cry
-[02:18.350]Первое свидание последней весны
-[02:18.350]The first date of the last spring
-[02:21.070]Сколько боли в мире есть и все от любви
-[02:21.070]How much pain in the world all from love
-[02:23.890]Девочки, ножи, автобусы, поезда
-[02:23.890]Girls, knives, buses, trains
-[02:27.240]Это все весна
-[02:27.240]It's all spring`
+const lrcStr = `[00:01.01]Alena Sergeevna Shvetsova
+[00:02.01]Швецова Алёна Сергеевна
+[00:00.84]Я приду, когда зацветет весна
+[00:00.84]I'll come when spring blooms
+[00:03.56]И покрасит в розовый собой облака
+[00:03.56]And it paints the clouds pink with itself
+[00:06.38]Соберу букет полевых цветов
+[00:06.38]I'll gather a bouquet of wildflowers
+[00:09.31]И спрячу
+[00:09.31]And hide
+[00:12.13]И мое сердечко болит от любви
+[00:12.13]And my heart aches from love
+[00:14.85]Просто слушай, ничего мне не говори
+[00:14.85]Just listen, don't say anything to me
+[00:17.67]Первое свидание последней весны
+[00:17.67]The first date of the last spring
+[00:20.38]Я плачу
+[00:20.38]I cry
+[00:23.31]Я приду, когда зацветет весна
+[00:23.31]I'll come when spring blooms
+[00:26.03]И покрасит в розовый собой облака
+[00:26.03]And it paints the clouds pink with itself
+[00:28.85]Соберу букет полевых цветов
+[00:28.85]I'll gather a bouquet of wildflowers
+[00:31.77]И спрячу
+[00:31.77]And hide
+[00:34.49]И мое сердечко болит от любви
+[00:34.49]And my heart aches from love
+[00:37.31]Просто слушай, ничего мне не говори
+[00:37.31]Just listen, don't say anything to me
+[00:40.13]Первое свидание последней весны
+[00:40.13]The first date of the last spring
+[00:42.85]Я плачу
+[00:42.85]I cry
+[00:45.67]В городе вчера зацвела сирень
+[00:45.67]Lilacs bloomed in the city yesterday
+[00:45.67]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+[00:48.49]Сыпет лепестки и ____ тень
+[00:48.49]Scattering petals and casting a shadow
+[00:51.21]Словно в аниме заблестят(shine) зрачки
+[00:51.21]Pupils shine like in an anime
+[00:54.55]Невзаимно, ты прости (вз = "co-","cooperate")
+[00:54.55]Unrequited, please forgive
+[00:54.55]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+[00:56.85]В питерской квартире кто-то _____ ножи
+[00:56.85]In Petersburg apartment some sharpens(точно) knives
+[00:59.78]Будущая жертва собирает цветы
+[00:59.78]The future victim(жер- sacrifice) gathers flowers
+[01:02.49]Встреча их случится, только ты подожди
+[01:02.49](Случайность happens to)(Случай Fall) Zufall
+[01:02.49]Their meeting will happen, just wait
+[01:05.84]Подожди
+[01:05.84]Wait
+[01:08.14]Я приду, когда зацветёт весна
+[01:08.14]I'll come when spring blooms
+[01:10.85]И покрасит в розовый собой облака
+[01:10.85]And it paints the clouds pink with itself
+[01:13.67]Соберу букет полевых цветов
+[01:13.67]I'll gather a bouquet of wildflowers
+[01:16.39]И спрячу
+[01:16.39]And hide
+[01:19.42]И моё сердечко болит от любви
+[01:19.42]And my heart aches from love
+[01:22.14]Просто слушай, ничего мне не говори
+[01:22.14]Just listen, don't say anything to me
+[01:24.96]The first date of the last spring
+[01:24.96]Первое свидание последней весны
+[01:27.78]Я плачу
+[01:27.78]I cry
+[01:30.60]Сколько обнимал, сколько целовал
+[01:30.60]How many hugs, how many kisses
+[01:30.60]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+[01:33.42]_____ рисунками холодный подвал
+[01:33.42]Decorating(у-=to add) a cold basement with drawings
+[01:36.24]Что же ты кричишь, не давая спать
+[01:36.24]Why are you screaming not letting me sleep
+[01:39.38]Ведь тебе не убежать
+[01:39.38](since)You cannot escape(Убрать wegbringen)
+[01:39.38]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+[01:41.89]Постучалась _____C сирень в окно
+[01:41.89]Lilac branches knocked on the window
+[01:44.60]Сыпет лепестки на бетонный пол
+[01:44.60]Scattering petals on the concrete floor
+[01:47.32]Сколько слез и радости ты нам принесла
+[01:47.32]How many tears and joy you brought us
+[01:50.66]Долгожданная весна
+[01:50.66]Long-awaited spring
+[01:55.89]Я приду, когда зацветёт весна
+[01:55.89]I'll come when spring blooms
+[01:58.60]И покрасит в розовый собой облака
+[01:58.60]And it paints the clouds pink with itself
+[02:01.43]Соберу букет полевых цветов
+[02:01.43]I'll gather a bouquet of wildflowers
+[02:04.25]И спрячу
+[02:04.25]And hide
+[02:06.96]And my heart aches from love
+[02:06.96]И мое сердечко болит от любви
+[02:09.89]Просто слушай, ничего мне не говори
+[02:09.89]Just listen, don't say anything to me
+[02:12.71]Первое свидание последней весны
+[02:12.71]The first date of the last spring
+[02:15.43]Я плачу
+[02:15.43]I cry
+[02:18.35]Первое свидание последней весны
+[02:18.35]The first date of the last spring
+[02:21.07]Сколько боли в мире есть и все от любви
+[02:21.07]How much pain in the world all from love
+[02:23.89]Девочки, ножи, автобусы, поезда
+[02:23.89]Girls, knives, buses, trains
+[02:27.24]Это все весна
+[02:27.24]It's all spring`
